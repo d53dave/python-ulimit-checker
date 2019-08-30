166a30922a69b352785a6f9001679e99dc7e9d39
David Sere <dave@d53dev.net>
2019-08-30 15:53:20 +0200

    Allow picking up test params from environment

--- fd.py
+++ fd.py
@@ -19,12 +19,12 @@ def create_files(num_files: int) -> int:
     return num_files
 
 
-def run_fd_check():
-    num_files = int(sys.argv[1])
+def run_fd_check(**kwargs):
+    num_files = int(kwargs['num_files'])
     print('Running fd check with number of files:', num_files)
     created_file_count = create_files(num_files)
     print('fd.py successfully created', created_file_count, 'files.')
 
 
 if __name__ == '__main__':
-    run_fd_check()
+    run_fd_check(num_files=sys.argv[1])
--- ulimit-checker.py
+++ ulimit-checker.py
@@ -1,10 +1,24 @@
 import fd
+import os
+
+from typing import Dict, Any
 
 check_fns = [fd.run_fd_check]
 
+
+def get_opts_from_env() -> Dict[str, Any]:
+    return {
+        'num_files': os.getenv('NUM_FILES', 10000),
+    }
+
+
 if __name__ == "__main__":
-    print('Starting ulimit test suite with fns:',
-          list(map(lambda fn: fn.__name__, check_fns)))
+    print('Starting ulimit test suite')
+
+    opts = get_opts_from_env()
+
     for fn in check_fns:
-        fn()
+        print('Running', fn.__name__)
+        fn(**opts)
+
     print('Test suite finished.')
